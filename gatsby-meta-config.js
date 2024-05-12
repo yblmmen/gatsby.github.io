1ba9536f5d9663eda91b76fe75a1a15e3a2da666
yblmmen <161982180+yblmmen@users.noreply.github.com>
2024-05-12 20:30:20 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `home`,
+  description: `임현우의 기술노트`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://superb-piroshki-842649.netlify.app/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `임현우`,
     bio: {
-      role: `개발자`,
+      role: `엔지니어`,
       description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: ``, // `https://github.com/yblmmen`,
+      linkedIn: ``, // `https://github.com/yblmmen`,
+      email: ``, // `yblmmen@gmail.com`,
     },
   },
 
@@ -44,7 +44,7 @@ module.exports = {
       // ========================================================
       {
         date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        activity: '네트워크 클라우드 인프라',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
           github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
@@ -72,15 +72,15 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '기술노트',
         description:
           '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
         techStack: ['gatsby', 'react'],
         thumbnailUrl: 'blog.png',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: 'https://github.com/yblmmen/.github.io',
+          github: 'https://github.com/yblmmen/.github.io',
+          demo: 'https://superb-piroshki-842649.netlify.app/,
         },
       },
     ],
