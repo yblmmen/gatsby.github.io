2e0b06670635f88969d6e38bab7c3b6b84cf8c9b
yblmmen <161982180+yblmmen@users.noreply.github.com>
2024-05-12 20:22:03 +0900

    Update index.js

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -9,9 +9,8 @@ function PageFooter({ author, githubUrl }) {
         &nbsp;
         <a href={githubUrl}>{author}</a>
         &nbsp;powered by
-        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
-          &nbsp;zoomkoding-gatsby-blog
-        </a>
+        <a href="https://superb-piroshki-842649.netlify.app/">
+          &nbsp;https://superb-piroshki-842649.netlify.app      </a>
       </p>
     </footer>
   );
