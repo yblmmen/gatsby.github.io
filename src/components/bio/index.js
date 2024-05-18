30f30f4f3d3c0832cfaa76461cafeb0338e09016
yblmmen <161982180+yblmmen@users.noreply.github.com>
2024-05-18 10:50:21 +0900

    Update index.js

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -12,11 +12,10 @@ function Bio({ author, language = 'ko' }) {
       {language === 'ko' ? (
         <div className="introduction korean">
           <p className="title">
-            안녕하세요.
             <br />
             <ReactRotatingText items={bio.description} />
             <br />
-            {bio.role} <strong>{name}</strong>입니다.
+            {bio.role} <strong>{name}</strong>
             <br />
           </p>
           <div className="social-links">
